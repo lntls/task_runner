806b3a8edb3f87feb41bfbbb9361ad63eb2f6a27
lntls <lntls@outlook.com>
2022-09-28 14:56:45 +0800

    export OnStart, OnStop

--- lib/task_runner.dart
+++ lib/task_runner.dart
@@ -1,5 +1,5 @@
 library task_runner;
 
 export 'src/runner.dart';
-export 'src/isolate.dart' show IsolateService, Task, OnCreated, OnClosed;
+export 'src/isolate.dart' show IsolateService, Task, OnStart, OnStop;
 
